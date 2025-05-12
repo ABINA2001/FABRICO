3b58991e47eca47716fecb5c6e3df2f97b36cfaf
ABINA2001 <139677249+ABINA2001@users.noreply.github.com>
2025-05-12 21:46:00 +0530

    Added Spring Boot base files

---
+++ src/main/java/com/fabrico/fabrico/FabricoApplication.java
@@ -0,0 +1,13 @@
+package com.fabrico.fabrico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FabricoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabrico/fabrico/FabricoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabrico.fabrico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
